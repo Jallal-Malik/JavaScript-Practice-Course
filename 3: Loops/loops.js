092a5a035df6f5d02bf050e5c186aff967550d3d
Jallal Developer <jallalmalik085@gmail.com>
2024-02-01 16:46:19 +0000

    Loops

---
+++ 3: Loops/loops.js
@@ -0,0 +1,57 @@
+// console.log("Good Morning")
+
+// let a = 1
+// console.log(a)
+// console.log(a + 1)
+// console.log(a + 2)
+// console.log(a + 3)
+// console.log(a + 4)
+// console.log(a + 5) // Simple Method to print Numbers
+
+// let a = 1
+
+// for (let i = 0; i < 100; i++) {
+//     const element = a + i;
+//     console.log(element)
+// } // For-Loop Method to Print 100 Numbers
+
+
+// Forin-Loop // Used for Objects
+
+let obj = {
+    name: "Jallal",
+    id: 180,
+    class: 12
+}
+
+// for (const key in obj) {
+//         // const element = key; // For Printing Keys
+//         // console.log(element)
+//         const element = obj[key]; // For Printing Values
+//         console.log(key, element)
+// }
+
+
+// Forof-Loops // Used Iterators / for strings & Arrays
+
+// for (const iterator of "Jallal") {
+//     console.log(iterator)
+// }
+
+
+// While-Loop
+
+// let i = 0
+// while (i<5) {
+//     console.log(i);
+//     i++;
+// }   // While-Loop Method
+
+
+// Do-While-Loop
+
+let a = 10
+do {
+    console.log(a);
+    a++;
+} while (a<6);
