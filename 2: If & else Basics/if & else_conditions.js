808241082980298342ed1563e3b5141f69bc9374
Jallal Developer <jallalmalik085@gmail.com>
2024-02-01 16:45:49 +0000

    if & else

---
+++ 2: If & else Basics/if & else_conditions.js
@@ -0,0 +1,93 @@
+// console.log("Hey this is basic of if & else")
+
+// Basic if & else
+
+// let a = 25
+// let a = 15
+
+// if(a>18) {
+//     console.log("You are Write")
+// }else {
+//     console.log("You are not Write")
+// }
+
+// if(a>18) {
+//     console.log("Your number is is greater than 18")
+// }else {
+//     console.log("Your number is less than 18")
+// }
+
+// Basic Operators
+
+let age = 18
+let graceFull = 2
+
+// age += graceFull // Assignment Operator
+
+// console.log(age + graceFull)
+// console.log(age)
+// console.log(age - graceFull)
+// console.log(age * graceFull)
+// console.log(age / graceFull)
+// console.log(age ** graceFull)
+// console.log(age % graceFull)
+
+// Comparison Operators
+
+// let equal1 = 18
+let equal2 = 17
+
+// if(equal1 == 18) {
+//     console.log("your answer is perfectly correct")
+// }
+
+// if(equal2 != 18) {
+//     console.log("Your answer is correct")
+// }
+
+// let ab = "18"
+// let bc = 18
+
+// if(ab === bc) {
+//     console.log("Your answer is perfectly correct")
+// }else {
+//     console.log("Your answer is not perfectly correct")
+// }
+
+// let eq1 = 18
+// let eq2 = 18
+
+// if(eq1 !== eq2) {
+//     console.log("Your answer is perfectly correct")
+// }else {
+//     console.log("Your answer is not perfectly correct")
+// }
+
+
+// Comments
+
+/* 
+Hy, How are you!
+I am fine
+*/ // This is multiLine Comments
+
+// If,else Ladder
+
+// let a = 18
+
+// if(a != 18) {
+//     console.log("You are good");
+// }else if(a == 18) {
+//     console.log("You are very good")
+// }else {
+//     console.log("You are not good")
+// } // This is if,else Ladder
+
+
+// Ternary Operators
+
+let a = 4
+let b = 6
+
+let c = a < b ? b - a : a - b
+console.log(c); // Ternary Operator  // c = condition check ? true : false
