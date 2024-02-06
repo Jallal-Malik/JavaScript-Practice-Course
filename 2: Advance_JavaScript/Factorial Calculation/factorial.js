79106da6dd0747f88cd49c8a3bb483c97234ef41
Jallal Developer <jallalmalik085@gmail.com>
2024-02-06 21:03:43 +0000

    Calculate the Factorial

--- 2: Advance_JavaScript/Factorial Calculation/factorial.js
+++ 2: Advance_JavaScript/Factorial Calculation/factorial.js
@@ -1,14 +1,14 @@
 let a  = 6
 
-// function factorial(number) {
-//     let arr = Array.from(Array(number + 1).keys())
-//     // console.log(arr.slice(1))
-//     let c= arr.slice(1).reduce((a, b) => {
-//         return a * b
-//     })
-//     console.log(c)
-// }
-// factorial(a)
+function factorial(number) {
+    let arr = Array.from(Array(number + 1).keys())
+    // console.log(arr.slice(1))
+    let c= arr.slice(1).reduce((a, b) => {
+        return a * b
+    })
+    console.log(c)
+}
+factorial(a)
 
 
 // function factorial(number) {
@@ -19,12 +19,12 @@ let a  = 6
 // }
 // factorial(a)
 
-function facFor(number) {
-    let fac = 1;
+// function facFor(number) {
+//     let fac = 1;
 
-    for (let index = 1; index <= number; index++) {
-        fac = fac * index       
-    }
-    return fac
-}
-console.log(facFor(a))
+//     for (let index = 1; index <= number; index++) {
+//         fac = fac * index       
+//     }
+//     return fac
+// }
+// console.log(facFor(a))
