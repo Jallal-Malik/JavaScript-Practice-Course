f4e20c1ce02af7c99d55921f9169c36f121e1ca4
Jallal Developer <jallalmalik085@gmail.com>
2024-02-06 20:00:21 +0000

    Exercise-1

---
+++ 2: Advance_JavaScript/1: DOM/2: Practice Exercises/Exercise-1/practice-1.js
@@ -0,0 +1,15 @@
+/*
+1: Create h2 and put any text
+2: Access h2
+3: append another text to h2 element
+*/
+
+// 2: Access h2
+let h2 = document.querySelector("h2")
+console.log(h2)
+
+// 3: Append another text to h2 element
+h2.innerText = h2.innerText + " New Version"
+console.log(h2)
+
+// Exercise Solved.
