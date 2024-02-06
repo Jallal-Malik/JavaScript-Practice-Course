8aecf731b86c0aeee08b1ff0ea8460dc737283f5
Jallal Developer <jallalmalik085@gmail.com>
2024-02-06 20:01:44 +0000

    Exercise-4

---
+++ 2: Advance_JavaScript/1: DOM/2: Practice Exercises/Exercise-4/practice-4.js
@@ -0,0 +1,10 @@
+/*
+1: Create a paragraph tag in Html
+2: Assign a Class & some stlying
+3: add new class and append to paragraph tag
+*/
+
+let newClass = document.querySelector(".para")
+newClass.classList.add("para-class") // we use ClassList to add new class
+
+// If i use setAttribute to add class but class is changeable not to add new.
