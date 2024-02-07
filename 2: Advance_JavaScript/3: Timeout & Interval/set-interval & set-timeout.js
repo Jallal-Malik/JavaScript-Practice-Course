3e0d240c609a6db7de162e519082231131c13a93
Jallal Developer <jallalmalik085@gmail.com>
2024-02-07 19:54:21 +0000

    Set-Interval & Set-Timeout

---
+++ 2: Advance_JavaScript/3: Timeout & Interval/set-interval & set-timeout.js
@@ -0,0 +1,24 @@
+// Set Interval
+
+// setInterval(() => {  // Its run again and again
+//     document.write(" This line run after 3 seconds <br></br>" )
+// }, 3000);
+
+// setInterval(written, 2000);
+
+// function written() {
+//     document.getElementById("para").innerHTML += " This is Set Interval Method <br></br>"
+// }
+
+
+// Set Timeout
+
+// setTimeout(() => {  // Its run only one time
+//     document.write(" This line run after 3 seconds <br></br>" )
+// }, 3000);
+
+// setTimeout(written, 2000);
+
+// function written() {
+//     document.getElementById("para").innerHTML = "This is Set Timeout Method"
+// }
